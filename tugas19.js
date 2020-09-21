0c335c03fba590af49e2bf9d0a653d2854bad958
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-09-21 04:45:59 +0000

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,12 @@
+var a = "Semangat..."
+console.log(a.repeat(5))
+var b = "+"
+console.log(b.repeat(5))
+console.log(b.repeat(15))
+console.log(b.repeat(25))
+console.log(b.repeat(35))
+console.log(b.repeat(55))
+console.log(b.repeat(35))
+console.log(b.repeat(25))
+console.log(b.repeat(15))
+console.log(b.repeat(5))
